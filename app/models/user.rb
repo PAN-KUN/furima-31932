f2e7ba3bfba36f8835fff75dd91a1387dcfe6ce3
PAN-KUN <88.matsuda.yuto@gmail.com>
2021-02-27 11:07:27 +0900

    正規表現を用いたvalidationを再設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+      validates :first_name
+      validates :last_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
 end
