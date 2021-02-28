bf844cce55754bca8a0848a2e3acf8355746d6e7
PAN-KUN <88.matsuda.yuto@gmail.com>
2021-02-28 16:23:25 +0900

    userモデルの単体テストコード記述
    pry-rails 導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    email                  { Faker::Internet.free_email }
+    password               { 'aaa111' }
+    password_confirmation  { password }
+    nickname               { Faker::Name.initials(number: 3) }
+    last_name              { '高橋' }
+    first_name             { 'たかし' }
+    last_name_kana         { 'タカハシ' }
+    first_name_kana        { 'タカシ' }
+    birth_date             { Faker::Date.between(from: '1990-01-01', to: '2010-12-31') }
   end
 end
