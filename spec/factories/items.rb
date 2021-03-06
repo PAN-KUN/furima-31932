3dc758577c718339cb5d2af0294cb84696577d91
PAN-KUN <88.matsuda.yuto@gmail.com>
2021-03-06 21:21:50 +0900

    factories/itemsの記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name              { Faker::Name.initials(number: 6) }
+    info           { Faker::Lorem.sentence }
+    price             { 777 }
+    category_id       { 2 }
+    sales_status_id         { 2 }
+    shipping_fee_status_id   { 2 }
+    prefecture_id     { 2 }
+    scheduled_delivery_id   { 2 }
+    association :user
   end
 end
