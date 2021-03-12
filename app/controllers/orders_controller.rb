ba0dbd57eedc28f39b92ac6a205810dc519804de
PAN-KUN <88.matsuda.yuto@gmail.com>
2021-03-12 22:56:35 +0900

    OdersControllerの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
   def index
   end
 
   def create
   end
 
+  private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end
