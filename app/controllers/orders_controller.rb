2e9fb31cc82f75e52fa2576ea1c293b5987a4c3d
PAN-KUN <88.matsuda.yuto@gmail.com>
2021-03-18 08:59:35 +0900

    OrdersControllerの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,26 @@
 class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
+  
   def index
+    @item_order = ItemOrder.new
   end
 
   def create
+    @item_order = ItemOrder.new(order_params)
+    if @item_order.valid?
+      @item_order.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
   end
 
   private
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:item_order).permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id] )
   end
 end
