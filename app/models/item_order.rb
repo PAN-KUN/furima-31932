aa9d0c4a4c01aec33e90b9ebc60876cdc832cf4e
PAN-KUN <88.matsuda.yuto@gmail.com>
2021-03-25 21:09:07 +0900

    ItemOrderモデルの修正

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,6 +1,6 @@
 class ItemOrder
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :user_id, :item_id, :order_id, :token
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code,  format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
@@ -15,6 +15,7 @@ class ItemOrder
 
   def save
     Order.create(user_id: user_id, item_id: item_id)
+    order_id = Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, phone_number: phone_number, order_id: order_id)
   end
 end
