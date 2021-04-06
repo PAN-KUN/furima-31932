fcffd62b79ddeb01c8897c86f6fb638d0d602f97
PAN-KUN <88.matsuda.yuto@gmail.com>
2021-04-06 22:44:26 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.16.0'
 set :application, 'furima-31932'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:PAN_KUN/furima-31932.git'
+set :repo_url,  'git@github.com:PAN-KUN/furima-31932.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
